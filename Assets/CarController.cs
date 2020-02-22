c882d13933d255d1226dea3e79474d96198cf5e3
Shubhra <ssuvro22@outlook.com>
2020-02-22 00:48:21 -0800

    2.0 Add Car Controller

---
+++ Assets/CarController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    [SerializeField] private WheelJoint2D backWheel;
+
+    private JointMotor2D motorBack;
+
+    public float backwardSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = Input.GetAxis("Horizontal");
+        motorBack.motorSpeed = speed * backwardSpeed;
+        backWheel.motor = motorBack;
+
+
+    }
+}
